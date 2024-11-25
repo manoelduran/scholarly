9a7cb805c7e2f83d68a2b59cc3624f645d6a87b9
Manoel Cendon Duran <manoel.duran@hotmail.com>
2024-11-25 16:30:46 -0300

    feat: added authentication to questions routes

--- apps/tasks/src/questions/questions.controller.ts
+++ apps/tasks/src/questions/questions.controller.ts
@@ -6,30 +6,35 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { UpdateQuestionDto } from './dto/update-question.dto';
+import { CurrentUser, JwtAuthGuard, UserDocument } from '@app/common';
 
 @Controller('questions')
 export class QuestionsController {
   constructor(private readonly questionsService: QuestionsService) {}
-
+  @UseGuards(JwtAuthGuard)
   @Post()
-  async create(@Body() createQuestionDto: CreateQuestionDto) {
+  async create(
+    @Body() createQuestionDto: CreateQuestionDto,
+    @CurrentUser() user: UserDocument,
+  ) {
     return this.questionsService.create(createQuestionDto);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Get()
   async findAll() {
     return this.questionsService.findAll();
   }
-
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return this.questionsService.findOne(+id);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   async update(
     @Param('id') id: string,
@@ -37,7 +42,7 @@ export class QuestionsController {
   ) {
     return this.questionsService.update(+id, updateQuestionDto);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return this.questionsService.remove(+id);
