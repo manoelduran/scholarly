328decc7b0d02a42ef928fec645244a064295e30
Manoel Cendon Duran <manoel.duran@hotmail.com>
2024-11-28 21:04:19 -0300

    feat: added authenticated guard to all authenticated endpoints

--- apps/tasks/src/tasks.controller.ts
+++ apps/tasks/src/tasks.controller.ts
@@ -3,19 +3,26 @@ import {
   Controller,
   Get,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { JwtAuthGuard, Roles } from '@app/common';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Roles('admin')
   @Get()
   async list() {
     return await this.tasksService.find();
   }
+  @UseGuards(JwtAuthGuard)
+  @Roles('admin')
   @Post()
   @UsePipes(new ValidationPipe())
   create(@Body() data: CreateTaskDto) {
