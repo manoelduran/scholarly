2c431489f032707231bbabcee6c4cebfbbe7043d
Manoel Cendon Duran <manoel.duran@hotmail.com>
2024-12-02 12:58:17 -0300

    feat: integrated ollama

--- apps/task-processor/src/task-processor.service.ts
+++ apps/task-processor/src/task-processor.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Ollama } from 'ollama';
 import { CreateTaskDto } from 'apps/tasks/src/dto/create-task.dto';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class TaskProcessorService {
-  private readonly baseUrl = 'https://api.ollama.ai';
+  private readonly ollama = new Ollama({
+    host: this.configService.getOrThrow('OLLAMA_HOST'),
+  });
 
   constructor(private readonly configService: ConfigService) {}
   async generateTask(data: CreateTaskDto) {
     try {
-      console.log('Processing task:', data, this.baseUrl);
-      // const response = await axios.post(`${this.baseUrl}/generate`, {
-      //   prompt,
-      // });
-      // return response.data.result;
+      console.log('Processing task:', data);
+      const response = await this.ollama.chat({
+        model: 'llama3.2',
+        messages: [{ role: 'user', content: 'Why is the sky blue?' }],
+      });
+      console.log(response.message.content);
+
       return 'Task generated';
     } catch (error) {
       console.error('Error interacting with Ollama API:', error);
