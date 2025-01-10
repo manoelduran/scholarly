0cba34516d9e3cee5f9a1c8c0bc49752305223ab
Manoel Cendon Duran <manoel.duran@hotmail.com>
2025-01-09 22:26:31 -0300

    feat: notifications setup

--- apps/notifications/src/notifications.module.ts
+++ apps/notifications/src/notifications.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
+import { LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        MONGODB_URI: Joi.string().required(),
+        RABBITMQ_URI: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [NotificationsController],
   providers: [NotificationsService],
 })
